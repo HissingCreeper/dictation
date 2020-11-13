aa70267f7150b6522ab6023d7dd679be967b13ca
JerryFu <jerryjf0202@qq.com>
2020-11-13 22:06:30 +0800

    Update rate.

--- dictation.py
+++ dictation.py
@@ -11,6 +11,8 @@ import pyttsx3
 f = open('text.txt', 'r', encoding='UTF-8')
 line = True
 engine = pyttsx3.init()
+rate = engine.getProperty('rate')
+engine.setProperty('rate', rate - 100)
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
 while line:
